43cfdfb25039ee23108011e97ca89cf7c9422194
dhanu <dhanuaras@gmail.com>
2026-05-09 09:34:55 +0530

    emailservice

--- src/main/java/com/chirag/flex/service/EmailService.java
+++ src/main/java/com/chirag/flex/service/EmailService.java
@@ -11,14 +11,21 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendResetEmail(String to, String link){
+    public void sendResetEmail(String to, String link) {
+        try {
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setTo(to);
+            msg.setSubject("Reset Password");
+            msg.setText("Click here to reset your password: " + link);
 
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(to);
-        msg.setSubject("Reset Password");
+            mailSender.send(msg);
 
-        msg.setText("Click here to reset your password: " + link);
+            System.out.println("EMAIL SENT SUCCESSFULLY");
 
-        mailSender.send(msg);
+        } catch (Exception e) {
+            System.out.println("EMAIL FAILED: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException("Email sending failed");
+        }
     }
 }
