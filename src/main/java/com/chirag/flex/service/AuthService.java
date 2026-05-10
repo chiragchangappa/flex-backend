0be3d57033d01cc6dba75bdaa85f0ac4541dae23
dhanu <dhanuaras@gmail.com>
2026-05-10 12:33:27 +0530

    secureandauth

--- src/main/java/com/chirag/flex/service/AuthService.java
+++ src/main/java/com/chirag/flex/service/AuthService.java
@@ -61,7 +61,7 @@ public class AuthService {
         repo.save(u);
 
         // ✅ ONLY ONE CLEAN LINK
-        String link = "https://courageous-manatee-c59108.netlify.app/reset.html?token=" + token;
+        String link = "https://benevolent-mochi-599607.netlify.app/reset.html?token=" + token;
 
         emailService.sendResetEmail(email, link);
     }
